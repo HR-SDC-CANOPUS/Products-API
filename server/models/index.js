c988b18577e8de9abb0f2bc0e7073075d8229d4d
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-26 12:19:23 -0400

    Styles and related products queries complete

--- server/models/index.js
+++ server/models/index.js
@@ -16,58 +16,85 @@ module.exports = {
   },
 
   readProduct: function (productId) {
-    return pool
-      .connect()
-      .then((client) => {
-        const query = `SELECT products.id, products.name, products.slogan, products.description, products.category, products.default_price,
+    return pool.connect().then((client) => {
+      const query = `SELECT products.id, products.name, products.slogan, products.description, products.category, products.default_price,
             (
             SELECT array_to_json(array_agg(row_to_json(d)))
             FROM (
-              select feature, value FROM features
-              where features.current_product_id = products.id
+              SELECT feature, value FROM features
+              WHERE features.current_product_id = products.id
             ) d
-          ) as features
+          ) AS features
           FROM products
-          where products.id = $1 `;
-        const result = client.query(query, [productId]);
-        client.release();
-        return result;
-      })
-      .catch((err) => console.log(err));
+          WHERE products.id = $1 `;
+      return client
+        .query(query, [productId])
+        .then((res) => {
+          client.release();
+          return res;
+        })
+        .catch((err) => {
+          client.release();
+          throw err;
+        });
+    });
   },
 
   readStyles: function (productId) {
-    return pool
-      .connect()
-      .then((client) => {
-        const query = `SELECT row_to_json(t) 
-        FROM(
-          SELECT products.id, products.name, products.slogan, products.description, products.category, products.default_price,
+    return pool.connect().then((client) => {
+      const query = `SELECT row_to_json(t)
+        FROM (
+          SELECT products.id,
             (
             SELECT array_to_json(array_agg(row_to_json(d)))
             FROM (
-              select feature, value FROM features
-              where features.current_product_id = products.id
+              SELECT styles.style_id, styles.name, styles.original_price, styles.sale_price, styles.default_style, (
+                SELECT array_to_json(array_agg(row_to_json(d)))
+                FROM (
+                  SELECT photos.url, photos.thumbnail_url FROM photos
+                  WHERE photos.style_id = styles.style_id
+                ) d
+              ) AS photos,
+              (
+                SELECT json_object_agg(skus.size, skus.quantity) FROM skus
+                WHERE skus.style_id = styles.style_id
+              ) AS skus
+              FROM styles
+              WHERE styles.product_id = products.id
             ) d
-          ) as features
+          ) AS results
           FROM products
-          where products.id = $1 
+          WHERE products.id = $1
         ) t`;
-        return client
-          .query(query, [productId])
-          .then((res) => {
-            client.release();
-            return res;
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
+      return client
+        .query(query, [productId])
+        .then((res) => {
+          client.release();
+          return res;
+        })
+        .catch((err) => {
+          client.release();
+          throw err;
+        });
+    });
   },
 
-  readRelated: function (params, callback) {},
+  readRelated: function (productId) {
+    console.log("hello", productId);
+    return pool.connect().then((client) => {
+      const query = `select array_agg(related_products.related_product_id)
+      from related_products
+      where related_products.current_product_id = $1`;
+      return client
+        .query(query, [productId])
+        .then((res) => {
+          client.release();
+          return res.rows[0].array_agg;
+        })
+        .catch((err) => {
+          client.release();
+          throw err;
+        });
+    });
+  },
 };
-
-// GET /products/list Retrieves the list of products.
-// GET /products/:product_id = Returns all product level information for a specified product id.
-// GET /products/:product_id/styles Returns the all styles available for the given product.
-// GET /products/:product_id/related - Returns the id's of products related to the product specified.
